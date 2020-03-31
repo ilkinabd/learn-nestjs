e5df1a6b7ea3608229bbeba7f58eec5ea51ae0d5
xterminate <ilkin5@mail.ru>
2020-04-01 01:09:54 +0300

    removes app controller

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,43 +1,18 @@
-import { Controller, Get, Res, Query, Post, Body, Param } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
-import { FindAllCatsDto } from './dto/fins-all-cats.dto';
-import { CatDto } from './dto/cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
   @Get()
-  findAllCats(@Query() query): FindAllCatsDto {
-    let findAllCatsDto = new FindAllCatsDto();
-    let catDto = new CatDto();
-    catDto.name = 'pussy';
-    catDto.age = 2;
-    findAllCatsDto.message = 'All cats';
-    findAllCatsDto.cats = [];
-    findAllCatsDto.cats.push(catDto);
-    return findAllCatsDto;
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string, @Res() res: Response): void {
-    res.json({
-      message: 'Cat has found',
-      cats: [
-        {
-          id: parseInt(id),
-          name: 'pussy',
-          age: 2,
-        },
-      ],
-    });
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
   }
 
   @Post()
-  create(@Body() { name, age }: CreateCatDto, @Res() res: Response): void {
-    res.json({
-      message: 'Cat has been created',
-      name: name,
-      age: age,
-    });
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
   }
 }
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { CatDto } from './dto/cat.dto';
+import { Cat } from './interfaces/cat.interface';
 
 @Injectable()
 export class CatsService {
-  private readonly cats: CatDto[];
+  private readonly cats: Cat[] = [];
+
+  create(cat: Cat) {
+    this.cats.push(cat);
+  }
+
+  findAll(): Cat[] {
+    return this.cats;
+  }
 }
